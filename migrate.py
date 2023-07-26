4071df73020ca64002362e07f68af54f928f5798
jkurian146 <113949907+jkurian146@users.noreply.github.com>
2023-07-26 13:21:33 -0400

    Update migrate.py

--- migrate.py
+++ migrate.py
@@ -5,6 +5,14 @@ import csv
 def main():
     src_csv = sys.argv[1]
     dst_folder = sys.argv[2]
+    # join custom drives with unc paths
+    dst_temp = dst_folder.split("\\")
+    if "S:" == dst_temp[0]:
+        dst_temp[0] = "\\gothamtg.net\shares"
+    elif "T:" == dst_temp[0]:
+        dst_temp[0] = "\\cofs01.gothamtg.net\\templates"
+    dst_folder = "\\".join(dst_temp)
+    # convert csv to dictionary
     data_dict = {}
     with open(src_csv, 'r') as file:
         reader = csv.reader(file)
@@ -12,20 +20,24 @@ def main():
             key = row[0]
             value = row[1]
             data_dict[key] = value
-    for key,val in data_dict.items():
+    # build path recursivley using key,value pairs
+    # Path is built out when the constructed path key 
+    # has no cooresponding value (no parent directory)
+    for key in data_dict.items():
         endPoint = key
         rel_path = ""
         while(endPoint != None):
             rel_path = os.path.join(rel_path,endPoint)
             endPoint = data_dict.get(endPoint)
+        # path manipulation for accuracy
         path_sep = rel_path.split('\\')
         reversed_path_sep = reversed(path_sep)
         final_path = '\\'.join(reversed_path_sep)
+        # avoids duplicate operations and errors raised
         try:
             os.makedirs(os.path.join(dst_folder,final_path))
         except:
             print("already made " + os.path.join(dst_folder,final_path))
-        #print(rel_path)
 
 
 if __name__ == "__main__":
